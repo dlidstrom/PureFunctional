b7236975442172df3ce3ed6cd77ff48598dc71e6
Daniel Lidstrom <dlidstrom@gmail.com>
2018-07-02 07:05:43 +0200

    Some functions

--- Haskell/baby.hs
+++ Haskell/baby.hs
@@ -4,3 +4,82 @@ doubleSmallNumber x = if x > 100
     then x
     else x * 2
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky n = "You're out of luck, pal!"
+
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' a b
+    | a <= b = b
+    | otherwise = a
+
+initials :: String -> String -> String
+initials firstName lastName = [f] ++ ". " ++ [l] ++ "."
+    where f:_ = firstName
+          l:_ = lastName
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+calcBmis2 :: [(Double, Double)] -> [Double]
+calcBmis2 xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ case ls of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
+
+-- recursion
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x : replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
