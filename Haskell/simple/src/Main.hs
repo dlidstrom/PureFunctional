f7ca0e92ab9fc80e46a033948c3fc2ed350b3284
Daniel Lidstrom <dlidstrom@gmail.com>
2019-01-15 21:19:33 +0100

    Monads exercise

--- Haskell/simple/src/Main.hs
+++ Haskell/simple/src/Main.hs
@@ -1,5 +1,27 @@
 module Main where
 
+import System.IO (hFlush, stdout)
+import Control.Exception (catch, IOException)
+import Control.Monad (replicateM_)
+
+prompt :: String -> IO a -> IO a
+prompt x getter = do
+    putStr x
+    hFlush stdout
+    line <- getter
+    return line
+
+run :: IO ()
+run = do
+    message <- prompt "Enter message: " getLine
+    n <- prompt "Enter N: " readLn
+    replicateM_ n (putStrLn message)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+    catch run handler
+    where
+        handler :: IOException -> IO ()
+        handler ex = do
+            let msg = "An error occurred:" <> show ex
+            putStrLn msg
